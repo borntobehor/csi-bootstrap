1023ed17fe255361dc1b2a308725d844c5e97296
borntobehor <hleanghor3@gmail.com>
2025-09-03 21:05:42 +0700

    counter and fadeInOut

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -88,3 +88,25 @@
 
 // * aos animation on scroll
 AOS.init();
+
+// * counter
+$(".counter").each(function () {
+  var $this = $(this),
+    countTo = $this.attr("data-countto");
+  countDuration = parseInt($this.attr("data-duration"));
+  $({ counter: $this.text() }).animate(
+    {
+      counter: countTo
+    },
+    {
+      duration: countDuration,
+      easing: "linear",
+      step: function () {
+        $this.text(Math.floor(this.counter));
+      },
+      complete: function () {
+        $this.text(this.counter);
+      }
+    }
+  );
+});
